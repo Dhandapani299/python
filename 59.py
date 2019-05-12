ed212c3b36bf217292c4d555e22fa1a225aa5a1c
Dhandapani299 <49011974+Dhandapani299@users.noreply.github.com>
2019-05-12 15:59:47 +0700

    59int.py

--- 59.py
+++
@@ -1,6 +0,0 @@
-ch = int(input())
-count = 0
-while(ch>0):
-  ch = ch // 10
-  count = count + 1
-print(count)
---
+++ 59int.py
@@ -0,0 +1,6 @@
+cha = int(input())
+count = 0
+while(cha>0):
+  cha = cha // 10
+  count = count + 1
+print(count)
