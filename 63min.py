d908f4a27b864e6ef28aa432ef2e2a882c69630b
Dhandapani299 <49011974+Dhandapani299@users.noreply.github.com>
2019-05-12 16:14:14 +0700

    Update and rename 63.py to 63min.py

--- 63min.py
+++ 63min.py
@@ -1,6 +1,6 @@
 list = []
-num = int(input())
-for i in range(1,num+1):
+n = int(input())
+for i in range(1,n+1):
   a = int(input("Enter element:"))
   list.append(a)
 print("smallest element :",min(list))
